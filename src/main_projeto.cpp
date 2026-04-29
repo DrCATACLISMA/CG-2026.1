2f2304223ae1ef14321ca60211be846480010af0
Leticia <leticiaandradesouza@gmail.com>
2026-04-29 20:36:01 -0300

    Adicionando esfera vermelha

--- src/main_projeto.cpp
+++ src/main_projeto.cpp
@@ -5,22 +5,36 @@
 #include <iostream>
 #include <fstream>
 
+bool hit_sphere(const point3 &center, double radius, const ray &r)
+{
+    vec3 oc = center - r.origin();              // vetor da origem ate o centro
+    auto a = dot(r.direction(), r.direction()); // coeficientes da equação
+    auto b = -2.0 * dot(r.direction(), oc);
+    auto c = dot(oc, oc) - radius * radius;
+    auto discriminant = b * b - 4 * a * c; // discriminante da equação
+    return (discriminant >= 0);
+}
+
 // renderizando um gradiente
-color ray_color(const ray& r) {
+color ray_color(const ray &r)
+{
+    if (hit_sphere(point3(0, 0, -1), 0.5, r)) // se o raio atingir a esfera, retorna vermelho
+        return color(1, 0, 0);
 
     // unit_vector -> função para a transformação de vetor para o unitario
     // r.direction -> getter com retorno da direção do raio
     vec3 unit_direction = unit_vector(r.direction());
 
     // getter do componente y do vetor
-    auto a = 0.5*(unit_direction.y() + 1.0);
+    auto a = 0.5 * (unit_direction.y() + 1.0);
 
     // retorna uma cor
     // ===== PERGUNTAR PARA O RENER
-    return (1.0-a)*color(1.0, 1.0, 1.0) + a*color(0.5, 0.7, 1.0);
+    return (1.0 - a) * color(1.0, 1.0, 1.0) + a * color(0.5, 0.7, 1.0);
 }
 
-int main() {
+int main()
+{
 
     // criação de objeto da imagem
     std::ofstream imagem{"imagem_CG.ppm"};
@@ -37,7 +51,7 @@ int main() {
     // altura viewport
     double viewport_height = 2.0;
     // largura viewport
-    double viewport_width = viewport_height * (double(image_width)/image_height);
+    double viewport_width = viewport_height * (double(image_width) / image_height);
     auto camera_center = point3(0, 0, 0);
 
     // largura e altura da tela, percorre pixel por pixel, orientação da imagem
@@ -49,9 +63,7 @@ int main() {
     auto pixel_delta_v = viewport_v / image_height;
 
     // calcula a posição do canto superior esquerdo da viewport
-    auto viewport_upper_left = camera_center
-                             - vec3(0, 0, focal_length) 
-                             - viewport_u/2 - viewport_v/2;
+    auto viewport_upper_left = camera_center - vec3(0, 0, focal_length) - viewport_u / 2 - viewport_v / 2;
     // calcula o centro do pixel (0,0)
     /*
         1. COMEÇA NO CENTRO da câmera, camera_center (0,0,0)
@@ -73,11 +85,14 @@ int main() {
     // viewport_upper_left é o canto do pixel, não o centro.
     // lançar o raio pelo centro do pixel
 
-    imagem << "P3\n" << image_width << ' ' << image_height << "\n255\n";
+    imagem << "P3\n"
+           << image_width << ' ' << image_height << "\n255\n";
 
-    for (int j = 0; j < image_height; j++) {
+    for (int j = 0; j < image_height; j++)
+    {
         std::clog << "\rScanlines remaining: " << (image_height - j) << ' ' << std::flush;
-        for (int i = 0; i < image_width; i++) { // perguntar para o Rener
+        for (int i = 0; i < image_width; i++)
+        { // perguntar para o Rener
 
             // pixel00_loc é a posição do primeiro pixel, pixel_delta_u e pixel_delta_v espaço entre os pixels
             auto pixel_center = pixel00_loc + (i * pixel_delta_u) + (j * pixel_delta_v);
@@ -89,5 +104,5 @@ int main() {
         }
     }
 
-    std::clog << "\rDone.                 \n";
+    std::clog << "\rFeito.                 \n";
 }
