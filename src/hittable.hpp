76d222b8cc8f1fa2c2c81774524b33f026452cce
Leticia <leticiaandradesouza@gmail.com>
2026-04-29 21:09:42 -0300

    lista e abstração para objetos que podem ser atingindos por um raio & face normal e frontal

---
+++ src/hittable.hpp
@@ -0,0 +1,32 @@
+#ifndef HITTABLE_H
+#define HITTABLE_H
+
+#include "ray.hpp"
+
+class hit_record
+{
+public:
+  point3 p;
+  vec3 normal;
+  double t;
+  bool front_face;
+
+  void set_face_normal(const ray &r, const vec3 &outward_normal)
+  {
+    // Define a normal quando acerta a esfera
+    // IMPORTANTE: o `outward_normal` é unitário
+
+    front_face = dot(r.direction(), outward_normal) < 0;
+    normal = front_face ? outward_normal : -outward_normal;
+  }
+};
+
+class hittable
+{
+public:
+  virtual ~hittable() = default;
+
+  virtual bool hit(const ray &r, double ray_tmin, double ray_tmax, hit_record &rec) const = 0;
+};
+
+#endif
---
+++ src/sphere.hpp
@@ -0,0 +1,48 @@
+#ifndef SPHERE_H
+#define SPHERE_H
+
+#include "hittable.hpp"
+#include "vector.hpp"
+
+class sphere : public hittable
+{
+public:
+    sphere(const point3 &center, double radius) : center(center), radius(std::fmax(0, radius)) {}
+
+    bool hit(const ray &r, double ray_tmin, double ray_tmax, hit_record &rec) const override
+    {
+        vec3 oc = center - r.origin(); // vetor da origem ate o centro
+        auto a = r.direction().length_squared(); // coeficientes da equação
+        auto h = dot(r.direction(), oc);
+        auto h = dot(r.direction(), oc);
+        auto c = oc.length_squared() - radius * radius; 
+
+        auto discriminant = h * h - a * c; // discriminante da equação
+        if (discriminant < 0) // nao atingiu a esfera
+            return false;
+
+        auto sqrtd = std::sqrt(discriminant);
+
+        //PERGUNTAR PARA O RENER
+        auto root = (h - sqrtd) / a;
+        if (root <= ray_tmin || ray_tmax <= root)
+        {
+            root = (h + sqrtd) / a;
+            if (root <= ray_tmin || ray_tmax <= root)
+                return false;
+        }
+
+        rec.t = root;
+        rec.p = r.at(rec.t);
+        vec3 outward_normal = (rec.p - center) / radius;
+        rec.set_face_normal(r, outward_normal);
+
+        return true;
+    }
+
+private:
+    point3 center;
+    double radius;
+};
+
+#endif
